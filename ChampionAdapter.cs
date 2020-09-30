3cc9c760ad3c6554ab29c0f78f0579877efbad29
Jassiel <contact@ochrefox.net>
2020-09-30 17:30:01 -0500

    Agregar archivos de proyecto.

---
+++ ChampionAdapter.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using Android.Support.V7.Widget;
+using Android.Views;
+using Android.Widget;
+using FFImageLoading;
+
+namespace AppLoLRepo
+{
+    class ChampionAdapter : RecyclerView.Adapter
+    {
+        public event EventHandler<PostAdapterClickEventArgs> ItemClick;
+        public event EventHandler<PostAdapterClickEventArgs> ItemLongClick;
+        List<MainActivity.Champion> items;
+
+        public ChampionAdapter(List<MainActivity.Champion> championList)
+        {
+            items = championList;
+        }
+
+        // Create new views (invoked by the layout manager)
+        public override RecyclerView.ViewHolder OnCreateViewHolder(ViewGroup parent, int viewType)
+        {
+
+            //Setup your layout here
+            var id = Resource.Layout.row;
+            var itemView = LayoutInflater.From(parent.Context).
+                Inflate(id, parent, false);
+
+            var vh = new PostAdapterViewHolder(itemView, OnClick, OnLongClick);
+            return vh;
+        }
+
+        // Replace the contents of a view (invoked by the layout manager)
+        public override void OnBindViewHolder(RecyclerView.ViewHolder viewHolder, int position)
+        {
+            var item = items[position];
+
+            // Replace the contents of the view with that element
+            var holder = viewHolder as PostAdapterViewHolder;
+
+            // Get image and set
+            if (holder != null)
+            {
+                GetImage(item.Image, holder.ChampionImage);
+                // Set other properties
+                holder.ChampionName.Text = item.Name;
+                holder.ChampionLore.Text = item.Lore;
+            }
+        }
+
+        // Download champion images
+        async void GetImage(string imageUrl, ImageView imageView)
+        {
+            await ImageService.Instance.LoadUrl(imageUrl)
+                .Retry(3, 200)
+                .DownSample(400, 400)
+                .IntoAsync(imageView);
+        }
+
+        public override int ItemCount => items.Count;
+
+        void OnClick(PostAdapterClickEventArgs args) => ItemClick?.Invoke(this, args);
+        void OnLongClick(PostAdapterClickEventArgs args) => ItemLongClick?.Invoke(this, args);
+
+    }
+
+    public class PostAdapterViewHolder : RecyclerView.ViewHolder
+    {
+        public ImageView ChampionImage { get; set; }
+        public TextView ChampionName { get; set; }
+        public TextView ChampionLore { get; set; }
+
+
+        public PostAdapterViewHolder(View itemView, Action<PostAdapterClickEventArgs> clickListener,
+                            Action<PostAdapterClickEventArgs> longClickListener) : base(itemView)
+        {
+            // Get items and set them to properties
+            ChampionImage = itemView.FindViewById<ImageView>(Resource.Id.championImage);
+            ChampionName = itemView.FindViewById<TextView>(Resource.Id.championTextView);
+            ChampionLore = itemView.FindViewById<TextView>(Resource.Id.bodyTextView);
+
+            itemView.Click += (sender, e) => clickListener(new PostAdapterClickEventArgs { View = itemView, Position = AdapterPosition });
+            itemView.LongClick += (sender, e) => longClickListener(new PostAdapterClickEventArgs { View = itemView, Position = AdapterPosition });
+        }
+    }
+
+    public class PostAdapterClickEventArgs : EventArgs
+    {
+        public View View { get; set; }
+        public int Position { get; set; }
+    }
+}
---
+++ MainActivity.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.Design.Widget;
+using Android.Support.V7.App;
+using Android.Support.V7.Widget;
+using Android.Views;
+using Android.Widget;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace AppLoLRepo
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        public class Champion
+        {
+            public Champion(string name, string lore, string id, string image)
+            {
+                Name = name;
+                Lore = lore;
+                Id = id;
+                Image = image;
+            }
+
+            public string Name { get; set; }
+            public string Lore { get; set; }
+            public string Id { get; set; }
+            public string Image { get; set; }
+        }
+
+        private RecyclerView _recyclerView;
+        // List<string> championImageUrls = new List<string>();
+        private readonly List<Champion> _championList = new List<Champion>();
+
+
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+
+            _recyclerView = FindViewById<RecyclerView>(Resource.Id.recyclerView);
+            GetData();
+            SetupRecyclerView();
+        }
+
+        private void SetupRecyclerView()
+        {
+            _recyclerView.SetLayoutManager(new Android.Support.V7.Widget.LinearLayoutManager(_recyclerView.Context));
+            var championAdapter = new ChampionAdapter(_championList);
+            _recyclerView.SetAdapter(championAdapter);
+        }
+
+
+        private async void GetData()
+        {
+            // Get latest version
+            const string versionUrl = "https://ddragon.leagueoflegends.com/api/versions.json";
+            string latest;
+
+            using (var client = new HttpClient())
+            using (var response = client.GetAsync(versionUrl).Result)
+            using (var content = response.Content)
+            {
+                var result = await content.ReadAsStringAsync();
+                var json = JsonConvert.DeserializeObject<List<string>>(result);
+                latest = json[0];
+            }
+
+            // Get champion JSON
+            if (latest == null) return;
+            var championUrl = "https://ddragon.leagueoflegends.com/cdn/" + latest + "/data/en_US/champion.json";
+
+            using (var client = new HttpClient())
+            using (var response = client.GetAsync(championUrl).Result)
+            using (var content = response.Content)
+            {
+                var result = await content.ReadAsStringAsync();
+                var json = JObject.Parse(result);
+
+                const string baseUrl = "http://ddragon.leagueoflegends.com/cdn/img/champion/splash/";
+                const string end = "_0.jpg";
+
+                var championData = json["data"].ToList();
+
+                foreach (var token in championData)
+                {
+                    var champion = token.ToObject<JProperty>();
+                    if (champion == null) continue;
+                    //championImageUrls.Add(baseUrl + champion.Name + end);
+                    _championList.Add(new Champion(
+                        champion.Value["name"]?.ToString(),
+                        champion.Value["blurb"]?.ToString(),
+                        champion.Value["id"]?.ToString(),
+                        baseUrl + champion.Name + end
+                    ));
+                }
+            }
+        }
+    }
+}
